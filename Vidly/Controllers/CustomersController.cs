41dfb959ef83cec8c087ee73b34aee551e486eea
Thomas Cianfarani <thomas.ci83@gmail.com>
2021-12-08 14:50:55 +0100

    Initial commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        IEnumerable<Customer> customers = new List<Customer>
+        {
+            new Customer { Id = 0, Name = "James Harden" },
+            new Customer { Id = 1, Name = "Armoni Brooks" }
+        };
+
+        public ActionResult Index()
+        {
+            return View(customers);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customer = customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }  
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        IEnumerable<Movie> movie = new List<Movie>() {
+            new Movie() { Id = 0, Name = "House of Gucci" },
+            new Movie() { Id = 1, Name = "The Last Duel" },
+            new Movie() { Id = 2, Name = "Dune" },
+            new Movie() { Id = 3, Name = "Shang-chi" }
+        };
+
+        public ActionResult Index()
+        {
+            return View(movie);
+        }
+
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
