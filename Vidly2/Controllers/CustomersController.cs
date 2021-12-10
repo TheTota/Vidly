646f78cb9a4c74a911260455dd4e7335a441ad24
Thomas Cianfarani <thomas.ci83@gmail.com>
2021-12-10 16:33:46 +0100

    add validation on the side of the client

--- Vidly2/Controllers/CustomersController.cs
+++ Vidly2/Controllers/CustomersController.cs
@@ -36,6 +36,7 @@ namespace Vidly2.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -43,8 +44,19 @@ namespace Vidly2.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
--- Vidly2/Models/Customer.cs
+++ Vidly2/Models/Customer.cs
@@ -10,11 +10,12 @@ namespace Vidly2.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter customer's name")]
         [StringLength(255)]
         public string Name { get; set; }
 
         [Display(Name = "Date of birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
